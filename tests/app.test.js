798171b307a63c4929f635c490e2064f3df73726
OhSva <somkamolv@hotmail.com>
2020-07-19 00:09:57 +0700

    test strapi instace, we cannot have afterAll for now since we have .tmp/test.db problem

---
+++ tests/app.test.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { setupStrapi } = require('./helpers/strapi');
+
+// We're setting timeout because sometimes bootstrap can take 5-7 seconds (big apps)
+//jest.setTimeout(10000);
+
+/** this code is called once before any test is called */
+beforeAll(async done => {
+    await setupStrapi(); // singleton so it can be called many times
+    done();
+});
+
+
+it('strapi is defined', async done => {
+    expect(strapi).toBeDefined();
+    done();
+});
